461715a6902d481b76c786aed8f6ed2f769e2b2a
Lorenzofox3 <laurent34azerty@gmail.com>
2016-10-03 16:39:58 -0400

    add dummy logger

--- index.js
+++ index.js
@@ -26,6 +26,28 @@ const tours = require('./tours.json');
 const vehicules = require('./vehicules.json');
 
 const app = koa()
+  .use(function * (next) {
+      const start = Date.now();
+      try {
+        yield next;
+      } catch (e) {
+        const status = e.status || 500;
+        this.status = status;
+        if (status === 500) {
+          console.error(e)
+        } else {
+          this.body = {error: e.message || 'unknown error', error_description: e.error_description || 'no description'};
+        }
+      } finally {
+        const elapsed = Date.now() - start;
+        const {method, url, ip} = this.request;
+        const status = this.status;
+        const referrer = this.get('Referer') || '-';
+        const logLine = [status, (this.body && this.body.error) || '-', method, [elapsed, 'ms'].join(' '), url, ip, referrer].join(' | ');
+        console.log(logLine);
+      }
+    }
+  )
   .use(compress())
   .use(favicon(__dirname + '/public/dist/img/logo.jpg'))
   .use(conditional())
